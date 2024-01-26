84fe7197ed6f3c543412ddcdbce84a641d8ce927
mollinaca <mail@mollinaca.dev>
2024-01-26 23:45:00 +0900

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker compose run palserver python main.py || sudo docker-compose run palserver python main.py
+sudo docker compose run palserver python main.py & || sudo docker-compose run palserver python main.py &
